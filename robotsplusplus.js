6accf976faab36f9e141cbbe08f8d1f9749cf51f
Ryan Lieu <github-benzBrake@woai.ru>
2024-05-25 19:48:59 +0800

    feat: IP 查询 API 更换为 ZXINC

--- robotsplusplus.js
+++ robotsplusplus.js
@@ -1,32 +1,36 @@
 function showIpLocation() {
     $(".robotx-location").text("正在查询...");
-    $(".robotx-ip").each(function() {
+    $(".robotx-ip").each(function () {
         var myd = $(this);
-        $.ajax({
-            url: "https://ip.huomao.com/ip?ip=" + myd.text(),
-            type: 'get',
-            dataType: 'json',
-            success: function(str) {
-                data = eval(str);
-                myd.next().text(data.country + data.province + data.city + data.isp).css("color", "#BD6800");
-            },
-            error: function(e) {
+        fetch('https://ip.zxinc.org/api.php?ip=' + myd.text())
+            .then(response => response.text())
+            .then(data => {
+                // Parse the XML response
+                let parser = new DOMParser();
+                let xmlDoc = parser.parseFromString(data, "text/xml");
+
+                // Extract the location information
+                let location = xmlDoc.getElementsByTagName('location')[0].childNodes[0].nodeValue;
+                myd.next().text(location).css("color", "#BD6800");
+            })
+            .catch(error => {
                 myd.next().text("无该 IP 详细信息").css("color", "#f00");
-            }
-        });
+                console.error('Error:', error)
+            });
     });
 }
-$(document).ready(function() {
+
+$(document).ready(function () {
     $(".check-ip-location").click(showIpLocation);
-    $(".robotx-ip").click(function() {
+    $(".robotx-ip").click(function () {
         $('.search-ip').val($(this).data('ip'));
         $('.search-btn').trigger('click');
     });
-    $(".robotx-bot-name").click(function() {
+    $(".robotx-bot-name").click(function () {
         $('.search-bot').val($(this).data('bot'));
         $('.search-btn').trigger('click');
     });
-    $(".clear-search-ip").click(function() {
+    $(".clear-search-ip").click(function () {
         $('.search-ip').val("");
         $('.search-btn').trigger('click');
     });
